b7b438994e5135e30130c8ae404581d4bda8d0b9
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-04-22 12:55:44 -0300

    .

---
+++ JS2024/jsEventos/Eventos de Mouse/onclick/script.js
@@ -0,0 +1,8 @@
+function eventoAoClicar() { //fazendo uma função
+    alert("Você clicou no botão"); //alert popup - somente mensagem - com a mensagem para o user
+}
+
+
+function eventoDoubleClick(){ //fazendo uma função
+    alert("Você deu 2 clicks no botão"); //alert popup - somente mensagem - com a mensagem para o user
+}
--- JS2024/jsEventos/onclick/script.js
+++
@@ -1,3 +0,0 @@
-function eventoAoClicar() { //fazendo uma função
-    alert("Você clicou no botão"); //alert popup - somente mensagem - com a mensagem para o user
-}
