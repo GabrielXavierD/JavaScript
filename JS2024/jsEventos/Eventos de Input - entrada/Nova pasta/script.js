52eacc3e38532a9609a49414c40c02f20cd84ee3
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-04-24 10:29:28 -0300

    .

--- JS2024/jsEventos/Eventos de Input - entrada/Nova pasta/script.js
+++
@@ -1,6 +0,0 @@
-//evento a ser disparado assim que a página for redimensionada
-function redimensione() {  
-    document.getElementById("log").innerHTML += "redimensionei <br>";
-    /*+= pega oq já esta dentro de tal elemento e adiciona mais X
-    - ou seja adicionando algo ao que já existe dentro do elemento*/
-    }
