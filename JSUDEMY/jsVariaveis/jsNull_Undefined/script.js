a9d35a2d2821fae421326c2da83500b2beb1f108
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-06-09 19:09:07 -0300

    .

---
+++ JSUDEMY/jsVariaveis/jsNull_Undefined/script.js
@@ -0,0 +1,17 @@
+/* 
+Null = representa ausencia INTENCIONAL de um valor
+Undefined = pode ser um valor atribuido (atribuiu que a var 
+            não esta definida, ou seja, sem valor);
+            quando se tenta acessar algo que não existe;
+            ou de fato algo que não possui nenhum valor.
+*/ 
+
+let teste = null; //neste momento, ela é intencionalmente vazia
+
+let teste2 = undefined; //atribuindo
+let teste3 //não tem valor, logo, não é definida, é undefined
+
+
+console.log(null)
+console.log(teste2)
+console.log(teste3)
