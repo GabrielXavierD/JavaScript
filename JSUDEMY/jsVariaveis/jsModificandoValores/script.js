43b550794e077f1e680c4e2aa62ae776660de13c
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-06-09 19:08:52 -0300

    .

---
+++ JSUDEMY/jsVariaveis/jsModificandoValores/script.js
@@ -0,0 +1,16 @@
+let teste = null; 
+var teste2 = undefined; 
+
+console.log(teste);
+console.log(teste2);
+
+teste = "Var teste foi modificada! :D";
+teste2 = "Var teste2 foi modificada! =)";
+
+console.log(teste);
+console.log(teste2);
+
+teste = 10;
+teste2 = 12;
+console.log(teste);
+console.log(teste2);
