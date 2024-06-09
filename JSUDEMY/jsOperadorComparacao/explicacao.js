f7c926b412705f1d9d6e4e094bd18511c696b34b
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-06-09 19:51:19 -0300

    .

---
+++ JSUDEMY/jsOperadorComparacao/explicacao.js
@@ -0,0 +1,12 @@
+/*
+
+==   (igual a)
+===  (estritamente igual a / e do mesmo tipo)
+!=   (diferente de)
+!==  (estritamente diferente de = valores diferentes e tipos diferentes)
+>    (maior que)
+>=   (maior ou igual a)
+<    (menor que)
+<=   (menor ou igual a)
+
+*/
