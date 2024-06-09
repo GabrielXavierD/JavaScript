36c7cce223b583fbfab5f0c30fee7cde43c61345
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-06-09 19:48:34 -0300

    .

---
+++ JSUDEMY/jsIf_Else/Explicacao/explicacao.js
@@ -0,0 +1,25 @@
+if(condicao){ //SE SIM FAÇA
+
+}
+
+if(condicao){ //SE SIM FAÇA
+
+}
+else{ //SE NÃO FAÇA (é opcional colocar)
+
+}
+
+
+
+
+
+if(condicao){//SE SIM FAÇA
+
+}
+else if(condicao){ //SE NÃO SE
+
+}
+else{ //SE NÃO
+
+}
+
