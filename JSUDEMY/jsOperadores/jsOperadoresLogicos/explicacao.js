f07f012447fe01a49b00de46c2ebb2dc6fb84a0f
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-06-09 23:26:50 -0300

    .

---
+++ JSUDEMY/jsOperadores/jsOperadoresLogicos/explicacao.js
@@ -0,0 +1,19 @@
+/*
+
+&&  (AND/E lógico)
+||  (OR/OU lógico)
+!   (NOT/NÃO lógico)
+??  (Operador de coalescência nula)
+
+
+-Explicação de ??
+    O operador de coalescência nula (??) 
+    é usado para fornecer um valor padrão quando a 
+    expressão à esquerda é null ou undefined. 
+Por exemplo:
+
+let nome = null;
+let nomePadrao = "Usuário";
+let nomeExibido = nome ?? nomePadrao;       //nomeExibido será "Usuário"
+
+*/
