42271f0a403e44c306fde214ffaa3973ec0921d9
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-06-25 15:12:52 -0300

    .

---
+++ JSUDEMY/jsEstruturasDeRepeticao/explicacao.js
@@ -0,0 +1,22 @@
+/* Estruturas de repetição, tambem chamados de laços/loops são estruturas
+
+Permitem com que a gente faça a repetição de um comando ou de um bloco de códigos
+até atender a uma determinada condição ou seja até atingir um determinado ponto em que falamos que não existe mais
+a necessidade de repetição do código e onde utilizamos as estruturas de repetição.
+
+-laços esperam o critério de parada ou seja uma instrução que indica que a repetição
+do comando do bloco de códigos deve ser interrompida.
+
+-A ausência de um critério de parada implica no que chamamos de loop infinito resultando no travamento
+da aplicação
+
+Onde utilizamos:
+-Para percorrer arrays
+-Para percorreis objetos literais
+
+3 PRINCIPAIS ESTRUTURAS DE REPETIÇÃO
+- while
+- do while
+- for
+
+*/
