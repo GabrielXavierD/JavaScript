d667d6018c25d29a0eddded7ca4484b939c450a6
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-07-17 16:47:41 -0300

    .

--- JSUDEMY/Tratamento_De_Erros_try_catch_throw_finally/script.js
+++ JSUDEMY/Tratamento_De_Erros_try_catch_throw_finally/script.js
@@ -1,59 +1,65 @@
 /*
 É muito comum dentro das nossas aplicações existirem trechos de código suscetíveis a erros que geram
-exceção. Ou seja, aqueles erros que interrompem o processamento do script, que interrompem a lógica do
+exceção. Ou seja, exceções são aqueles erros que interrompem o processamento do script, que interrompem a lógica do
 algoritmo, fazendo com que a aplicação literalmente morra.
 
-Nesses casos geralmente o próprio interpretador da linguagem, no caso do JS, o browser, ele dispara para o usuário informações técnicas, mas aí, o usuário acaba ficando perdido
+Nesses casos geralmente o próprio interpretador da linguagem, no caso do JS, o browser, ele dispara para o usuário informações técnicas, mas aí, o usuário acaba ficando perdido...
 
 O TRATAMENTO DE ERROS
--É uma técnica que podemos empregar em algumas partes do nosso código para evitar que erros interrompam a nossa aplicação;
--O tratamento de erros é um dos pilares para implementação do paradigma de programação reativa
+-É uma técnica que podemos empregar em algumas partes do nosso código para evitar que erros interrompam a nossa aplicação (exceções);
+-O tratamento de erros é um dos pilares para implementação do paradigma de programação reativa;
 -É importante para deixar as aplicações mais seguras;
--Podemos registrar esses erros e até mesmo fazer com que a nossa aplicação se torne reativa a certas situações
--TRY CATCH FINALLY THROW
+-Podemos registrar esses erros e até mesmo fazer com que a nossa aplicação se torne reativa a certas situações;
+-TRY, CATCH, FINALLY e THROW;
 
+Exceção: erro que faz o processamento morrer
 
--Exceção: erro que faz o processamento morrer
 */
+
+
+
 
 // Array bidimensional - há 2 entradas no indice 1
 var video = []
+        video[1] = []                            // -> criando um array na posição 1 do array "Pai"
+          video[1]["Nome"] = "Naruto"             // -> criando indice/propriedade "Nome", no array 1 "Filho" e atribuindo "Naruto" a ele
+          video[1]["Categoria"] = "Anime"
 
-video[1] = []
-video[1]["Nome"] = "Naruto"
-video[1]["Categoria"] = "Anime"
+        
+document.write(video)
+document.write(video[1]["Nome"])          
 
 console.log(video)
 
 
-
-
 /*
-Função (ficticia, apenas para o ex.) para recuperar video do servidor
+Abaixo há uma função ficticia -apenas para o ex.- para recuperar video do servidor:
+
     Dentro dessa função, como seria para uma requisição http, poderia haver uma serie de erros ao realizar a requisição 
     Ex.: Pode ser que durante esse processo de requisição o vídeo não esteja mais disponível etc...
+
+    O que essa função faz?
+    -Estamos forçando um "erro" pra simular como seria a exceção, ele retorna uma exceção, um erro que interrompe nosso algoritmo.
+    
+    Como esse erro é gerado?
+    -Estamos tentando acessar um indice 0 que não existe no array original, o que causa um undefined, que é um erro, mas não uma exceção. Portanto, ao acessar o array "video", que seria um atributo de undefined (o "filho") causaremos uma exceção, ou seja, interrompe o algoritmo.
 
 
 function recuperaVideo(video) {
     logica
 
-    forçando um "erro" pra simular como seria, p/ retornar uma exceção, um erro que interrompa nosso algoritmo, so um ex.:
-
-    ao tentar acessar um atributo de undefined(indice 0, que n existe no array), o nome, teremos um erro, uma exceção, que interrompe o processmento do script
-
     console.log(video[0]["video"])
     console.log("A aplicação não morreu") 
 }
-
-recuperaVideo(naruto.mp4);
-*/
+recuperaVideo(video);
 
 
-
+*************************************************************************************************************************************************
 
 
 
-/* EXPLICAÇÃO
+EXPLICAÇÃO TRY, CATCH, THROW E FINALLY 
+-Podemos usar os 4 juntos, somente 2 deles etc...
 
 =TRY=
 -Try: tente
@@ -61,20 +67,18 @@ recuperaVideo(naruto.mp4);
 -Podemos utilizar quantos try's forem necessarios, sem limitações
 
 
-
 =TRY FINALLY=
 -Try: tente
 -Finally: finalmente (pode ser aplicado com o try e com o catch)
-TRY FINALLY: Apos TENTAR alguma coisa FINALIZE essa coisa de alguma forma
+TRY FINALLY: Apos TENTAR alguma coisa FINALIZE essa coisa de alguma forma.
 
-Caso aconteça algum erro no try, ele executa o código contido no FINALLY, mas, o erro continua sendo mostrado no console.
-Ou seja, a nossa aplicação ainda não está reativa a ponto de se recuperar de um erro, de uma excessão e continuar funcionando normalmente.
-
+Caso aconteça algum erro no TRY, ele executa o código contido no FINALLY, mas, o erro continua sendo mostrado no console.
+Ou seja, a nossa aplicação ainda não está reativa a ponto de se recuperar de um erro (de uma excessão) e continuar funcionando normalmente.
 
 
 =TRY CATCH=
 -Try: tente
--Catch(): pegar (recebe/captura o erro por parametro e o trata/faz uma ação fazendo com que o fluxo da aplicação não quebre)
+-Catch(): pegar (recebe o erro por parametro e faz uma ação, o que não deixa o fluxo da aplicação quebrar)
 
 
 =TRY CATCH FINALLY=
@@ -83,17 +87,53 @@ Ou seja, a nossa aplicação ainda não está reativa a ponto de se recuperar de
 -Finally: finalmente
 
 
+THROW
+-Throw: lançar algo
+
+-Ele vai um pouco em contramão em relação a ideia de tornar a aplicação reativa quando ocorrer algum erro, pode ser utilizado, mas 
+se quisermos tornar a aplicação reativa, não o utilizamos no tratamento de erros (junto com o CATCH etc...);
+-A partir dele nós podemos criar uma nova (new) instância do objeto Error() e forçar a exibição desse objeto, fazendo com que a aplicação seja interrompida;
+-Devemos indicar qual o local onde a exceção vai acontecer e podemos personalizar qual mensagem ela vai apresentar nesses locais (no console);
+-É geralmente utilizado no CATCH, caso seja necessario o interrompimento do script;
+    ->Mas pode ser colocado em qualquer local da aplicação, exemplo: a primeira linha do script, ja inicia com o erro, ou seja, 
+      não necessariamente inicia dentro do tratamento de erros e sim fora dele;
+    ->Mas, no caso do CATCH, nada impede que antes do interrompimento, seja feita uma ação de armazenamento desse erro no servidor, possibilitando que os desenvolvedores deem uma analisada nesse erro, evitando que esse comportamento caia no esquecimento.
+
+-Serve para lançar exceções dentro das nossas aplicações.
+
+Sintaxe:
+throw new Error()
 
 */
+
+
+
+/* 
+Ele vai forçar uma exceção dentro do código forçando a interrupção do script.
+O código abaixo, gera uma mensagem personalizada no erro apresentado no console.
+
+throw new Error("Iniciando o script forçando um erro  - Houve um erro estamos trabalhando nisso! :-)")
+
+CODIGO COMENTADO - POIS SE Ñ, NÃO FUNCIONA A FUNÇÃO ABAIXO:
+*/
+
 function recuperaVideo(video) {
 
     try {
-        //tudo que pode dar erro
+        //tudo que pode dar erro se coloca aq
         console.log(video[0]["video"])
     } catch(erro) {
-        //o erro continua acontecendo, porem, não teremos mais uma exceção que interromperá a logica do script, agora o tratamos
-        console.log(erro) //erro = não é o erro do try acontecendo, mas sim, é a string que contem o erro vindo do try, só estamos apresentando o que tem dentro dela, no caso, o erro vindo do try. 
+        /*o erro continua acontecendo, porem, não teremos mais uma exceção que interromperá a logica do script, agora o tratamos aq
+
+        erro (parametro do catch) = não é o erro do try acontecendo, mas sim, é a string que contem o erro vindo do try, só estamos apresentando o que tem dentro dela 
+        
+        -> aqui poderiamos implementar uma logica capaz de pegar o erro e encaminhar para o servidor - no caso, fiz a função tratarErro() - por ex. e relatar o erro p/ equipe de desenvolvimento 
+
+        */
+        tratarErro(erro) 
         console.log("Agora sim podemos tratar esse erro")
+        throw new Error("Houve um erro mas não se preocupe, estamos trabalhando nisso agora! :)")
+
     } finally {
         console.log("SEMPRE PASSA POR AQUI (try / catch)")
     }
@@ -103,3 +143,8 @@ function recuperaVideo(video) {
 
 recuperaVideo(video);
 
+
+function tratarErro(erro){
+    // logica para registrar o erro no servidor
+    console.log(erro)
+}
