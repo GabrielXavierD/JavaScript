16523f5a9462c725a27a2ea983f2c09817125326
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-07-23 12:06:06 -0300

    .

--- JSUDEMY/jsExibir/script.js
+++ JSUDEMY/jsExibir/script.js
@@ -18,3 +18,21 @@ console.log(texto2)
 let nome = prompt("Digite seu nome:")
 let comida = prompt("Digite sua comida favorita:")
 alert(nome + ", sua comida favorita é: " + comida)
+
+
+
+/*abre um popup com opção OK ou CANCELAR */
+let sair = confirm("Confirma ir para a página do Google?"); 
+
+
+/* Ok = 1 = verdadeiro 
+  Cancelar = 0 = falso  */
+
+if(sair == 1){
+  /*Se sim, window.location (janela do navegador) deve carregar tal página*/ 
+  window.location = "http://www.google.com.br";
+}
+
+
+/*Para abrir uma nova janela */
+sair = window.open("http://www.w3schools.com");
