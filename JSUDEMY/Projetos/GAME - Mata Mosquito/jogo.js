d6ce328b105c63b7553ded81ad8f1c168b9c3370
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-07-25 16:06:14 -0300

    .

--- JSUDEMY/Projetos/GAME - Mata Mosquito/jogo.js
+++ JSUDEMY/Projetos/GAME - Mata Mosquito/jogo.js
@@ -8,11 +8,10 @@ innerHeight e innerWidth
 usuario redimensione a tela (pois se ele redimensionar, talvez ele não veja o spawn das moscas, logo o valor de alt. e larg. deve ser atualizado constantemente p/ que o jogo se adapte a tela do usuario e assim ele continue vendo o spawn das moscas mesmo se ele redimensionar a tela). 
     ->Portanto, encapsulei essa logica de recuperação de alt./larg. em uma uma função, pois caso o usuario redimensione a tela o valor atual da altura e largura será atualizado atraves do evento onresize() do body que chamará a função
         ->Ou seja, toda vez que o user redimensionar a tela, a chamada da função será associada ao onresize(), que irá chamar a função ajustaTamanhoPalcoJogo() e atualizar os valores de altura e largura
+
+-Iniciando as variaveis em 0, pois seus valores serão atualizados dentro da função
+-se são declaradas dentro da propria função, entao tem escopo da função e não escopo global, então não podemos acessar seus valores atualizados globalmente (em um console.log() FORA da função, por ex.). Logo, devemos criar as variaveis antes da função, p/ podermos acessa-las globalmente com seus valores atualizados, pois, aí, dentro da função, estamos atualizando o valor de uma VARIAVEL GLOBAL e não de uma variavel criada dentro da propria função.
 */
-
-
-// Iniciando as variaveis em 0, pois seus valores serão atualizados dentro da função
-// se são declaradas dentro da propria função, entao tem escopo da função e não escopo global, então não podemos acessar seus valores atualizados globalmente (em um console.log() FORA da função, por ex.). Logo, devemos criar as variaveis antes da função, p/ podermos acessa-las globalmente com seus valores atualizados, pois, aí, dentro da função, estamos atualizando o valor de uma VARIAVEL GLOBAL e não de uma variavel criada dentro da propria função.
 var altura = 0;
 var largura = 0;
 
