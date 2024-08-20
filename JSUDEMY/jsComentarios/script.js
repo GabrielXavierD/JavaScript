3ae62dbdd8c80041bb11c6744998edc30b76fe58
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-08-20 14:36:27 -0300

    .

--- JSUDEMY/jsComentarios/script.js
+++
@@ -1,7 +0,0 @@
-// Desconsiderado pelo interpretador do JS
-// document.write("TAG SCRIPT DENTRO DO BODY (CARREGADO NO FINAL DO BODY)");
-
-
-/*Descrever, comentar, explicar código, instruções, documentação etc...
- Podemos comentar trechos para testar bugs -> processo de debugging*/
-
